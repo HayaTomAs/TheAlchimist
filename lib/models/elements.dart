85caa1ce57d15af7bffe9964f88d41a1e04a5ab7
Thomas Darmengeat <thomas.darmengeat@gmail.com>
2024-05-22 16:32:18 +0200

    Suppression des commentaires genant pour la gestion via interface Ajout d'elements

--- lib/models/elements.dart
+++ lib/models/elements.dart
@@ -9,7 +9,6 @@ class GameElement {
   GameElement(this.id, this.translationKey, this.imagePath,
       {this.discovered = false, required this.possibleCombinations, this.discoveryOrder});
 
-  // Méthode pour créer une copie de l'élément
   GameElement copy() {
     return GameElement(
       id,
@@ -22,7 +21,6 @@ class GameElement {
   }
 }
 
-// Créer une copie des éléments initiaux
 List<GameElement> getInitialElements() {
   return initialElements.map((e) => e.copy()).toList();
 }
@@ -56,200 +54,254 @@ final initialElements = <GameElement>[
     'atom',
     'element_atom',
     'assets/images/element_generic.png',
+    discovered: false,
     possibleCombinations: [
-      {'parent1Id': 'void', 'parent2Id': 'energy'},
+      {'parent1Id': 'void', 'parent2Id': 'energy'}
+    ],
+  ),
+  GameElement(
+    'element',
+    'element_element',
+    'assets/images/element_generic.png',
+    discovered: false,
+    possibleCombinations: [
+      {'parent1Id': 'atom', 'parent2Id': 'atom'}
     ],
   ),
   GameElement(
     'cellular',
     'element_cellular',
     'assets/images/element_generic.png',
+    discovered: false,
     possibleCombinations: [
-      {'parent1Id': 'atom', 'parent2Id': 'atom'},
+      {'parent1Id': 'element', 'parent2Id': 'time'}
     ],
   ),
   GameElement(
     'moon',
     'element_moon',
     'assets/images/element_generic.png',
+    discovered: false,
     possibleCombinations: [
-      {'parent1Id': 'atom', 'parent2Id': 'energy'},
+      {'parent1Id': 'stone', 'parent2Id': 'energy'}
     ],
   ),
   GameElement(
     'star',
     'element_star',
     'assets/images/element_generic.png',
+    discovered: false,
     possibleCombinations: [
-      {'parent1Id': 'time', 'parent2Id': 'energy'},
+      {'parent1Id': 'time', 'parent2Id': 'energy'}
     ],
   ),
   GameElement(
     'planet',
     'element_planet',
     'assets/images/element_generic.png',
+    discovered: false,
     possibleCombinations: [
-      {'parent1Id': 'moon', 'parent2Id': 'star'},
+      {'parent1Id': 'moon', 'parent2Id': 'star'}
     ],
   ),
   GameElement(
     'fire',
     'element_fire',
     'assets/images/element_generic.png',
+    discovered: false,
     possibleCombinations: [
-      {'parent1Id': 'hood', 'parent2Id': 'thunder'},
+      {'parent1Id': 'hood', 'parent2Id': 'thunder'}
     ],
   ),
   GameElement(
     'water',
     'element_water',
     'assets/images/element_generic.png',
+    discovered: false,
     possibleCombinations: [
-      {'parent1Id': 'wind', 'parent2Id': 'time'},
+      {'parent1Id': 'wind', 'parent2Id': 'time'}
     ],
   ),
   GameElement(
     'life',
     'element_life',
     'assets/images/element_generic.png',
+    discovered: false,
     possibleCombinations: [
-      {'parent1Id': 'water', 'parent2Id': 'cellular'},
+      {'parent1Id': 'water', 'parent2Id': 'cellular'}
     ],
   ),
   GameElement(
     'earth',
     'element_earth',
     'assets/images/element_generic.png',
+    discovered: false,
     possibleCombinations: [
-      {'parent1Id': 'cellular', 'parent2Id': 'cellular'},
+      {'parent1Id': 'cellular', 'parent2Id': 'cellular'}
     ],
   ),
   GameElement(
     'air',
     'element_air',
     'assets/images/element_generic.png',
+    discovered: false,
     possibleCombinations: [
-      {'parent1Id': 'void', 'parent2Id': 'atom'},
+      {'parent1Id': 'void', 'parent2Id': 'atom'}
     ],
   ),
   GameElement(
     'steam',
     'element_steam',
     'assets/images/element_generic.png',
+    discovered: false,
     possibleCombinations: [
-      {'parent1Id': 'fire', 'parent2Id': 'water'},
+      {'parent1Id': 'fire', 'parent2Id': 'water'}
     ],
   ),
   GameElement(
     'mud',
     'element_mud',
     'assets/images/element_generic.png',
+    discovered: false,
     possibleCombinations: [
-      {'parent1Id': 'water', 'parent2Id': 'earth'},
+      {'parent1Id': 'water', 'parent2Id': 'earth'}
     ],
   ),
   GameElement(
     'wind',
     'element_wind',
     'assets/images/element_generic.png',
+    discovered: false,
     possibleCombinations: [
-      {'parent1Id': 'air', 'parent2Id': 'air'},
+      {'parent1Id': 'air', 'parent2Id': 'air'}
     ],
   ),
   GameElement(
     'stone',
     'element_stone',
     'assets/images/element_generic.png',
+    discovered: false,
     possibleCombinations: [
       {'parent1Id': 'air', 'parent2Id': 'earth'},
+      {'parent1Id': 'dust', 'parent2Id': 'time'}
     ],
   ),
   GameElement(
     'lava',
     'element_lava',
     'assets/images/element_generic.png',
+    discovered: false,
     possibleCombinations: [
-      {'parent1Id': 'fire', 'parent2Id': 'stone'},
+      {'parent1Id': 'fire', 'parent2Id': 'stone'}
     ],
   ),
   GameElement(
     'atmospher',
     'element_atmospher',
     'assets/images/element_generic.png',
+    discovered: false,
     possibleCombinations: [
-      {'parent1Id': 'planet', 'parent2Id': 'air'},
+      {'parent1Id': 'planet', 'parent2Id': 'air'}
     ],
   ),
   GameElement(
     'cloud',
     'element_cloud',
     'assets/images/element_generic.png',
+    discovered: false,
     possibleCombinations: [
-      {'parent1Id': 'atmospher', 'parent2Id': 'wind'},
+      {'parent1Id': 'atmospher', 'parent2Id': 'wind'}
     ],
   ),
   GameElement(
     'rain',
     'element_rain',
     'assets/images/element_generic.png',
+    discovered: false,
     possibleCombinations: [
-      {'parent1Id': 'cloud', 'parent2Id': 'wind'},
+      {'parent1Id': 'cloud', 'parent2Id': 'wind'}
     ],
   ),
   GameElement(
     'thunder',
     'element_thunder',
     'assets/images/element_generic.png',
+    discovered: false,
     possibleCombinations: [
-      {'parent1Id': 'cloud', 'parent2Id': 'energy'},
+      {'parent1Id': 'cloud', 'parent2Id': 'energy'}
     ],
   ),
   GameElement(
     'ocean',
     'element_ocean',
     'assets/images/element_generic.png',
+    discovered: false,
     possibleCombinations: [
-      {'parent1Id': 'rain', 'parent2Id': 'planet'},
+      {'parent1Id': 'rain', 'parent2Id': 'planet'}
     ],
   ),
   GameElement(
     'grass',
     'element_grass',
     'assets/images/element_generic.png',
+    discovered: false,
     possibleCombinations: [
-      {'parent1Id': 'planctoon', 'parent2Id': 'planet'},
+      {'parent1Id': 'planctoon', 'parent2Id': 'planet'}
     ],
   ),
   GameElement(
     'planctoon',
     'element_planctoon',
     'assets/images/element_generic.png',
+    discovered: false,
     possibleCombinations: [
-      {'parent1Id': 'ocean', 'parent2Id': 'life'},
+      {'parent1Id': 'ocean', 'parent2Id': 'life'}
     ],
   ),
   GameElement(
     'oxygen',
     'element_oxygen',
     'assets/images/element_generic.png',
+    discovered: false,
     possibleCombinations: [
-      {'parent1Id': 'atmospher', 'parent2Id': 'planctoon'},
+      {'parent1Id': 'atmospher', 'parent2Id': 'planctoon'}
     ],
   ),
   GameElement(
     'animal',
     'element_animal',
     'assets/images/element_generic.png',
+    discovered: false,
     possibleCombinations: [
-      {'parent1Id': 'oxygen', 'parent2Id': 'planet'},
+      {'parent1Id': 'oxygen', 'parent2Id': 'planet'}
     ],
   ),
   GameElement(
     'hood',
     'element_hood',
     'assets/images/element_generic.png',
+    discovered: false,
     possibleCombinations: [
-      {'parent1Id': 'grass', 'parent2Id': 'time'},
+      {'parent1Id': 'grass', 'parent2Id': 'time'}
+    ],
+  ),
+  GameElement(
+    'dust',
+    'element_dust',
+    'assets/images/element_generic.png',
+    discovered: false,
+    possibleCombinations: [
+      {'parent1Id': 'element', 'parent2Id': 'energy'},
+      {'parent1Id': 'stone', 'parent2Id': 'wind'}
+    ],
+  ),
+  GameElement(
+    'tornado',
+    'element_tornado',
+    'assets/images/element_generic.png',
+    discovered: false,
+    possibleCombinations: [
+      {'parent1Id': 'wind', 'parent2Id': 'wind'}
     ],
   ),
 ];
