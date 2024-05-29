cc38d215f3719e2bd70b0af5e89e0977878c49f9
Thomas Darmengeat <thomas.darmengeat@gmail.com>
2024-05-29 21:21:05 +0200

    Premier palier de 10 elements

--- lib/models/elements_list.dart
+++ lib/models/elements_list.dart
@@ -1,46 +1,12 @@
-import '/models/elements_base.dart';
-
-final initialElements = <GameElement>[
-  GameElement(
-    'void',
-    'element_void',
-    'assets/images/element_generic.png',
-    discovered: true,
-    discoveryOrder: 0,
-    possibleCombinations: [],
-  ),
-  GameElement(
-    'time',
-    'element_time',
-    'assets/images/element_generic.png',
-    discovered: true,
-    discoveryOrder: 1,
-    possibleCombinations: [],
-  ),
-  GameElement(
-    'energy',
-    'element_energy',
-    'assets/images/element_generic.png',
-    discovered: true,
-    discoveryOrder: 2,
-    possibleCombinations: [],
-  ),
-  GameElement(
-    'atom',
-    'element_atom',
-    'assets/images/element_generic.png',
-    discovered: false,
-    possibleCombinations: [
-      {'parent1Id': 'void', 'parent2Id': 'energy'}
-    ],
-  ),
-  GameElement(
-    'gas',
-    'element_gas',
-    'assets/images/element_generic.png',
-    discovered: false,
-    possibleCombinations: [
-      {'parent1Id': 'void', 'parent2Id': 'atom'}
-    ],
-  ),
+import '/models/elements_base.dart';final initialElements = <GameElement>[
+GameElement('void','element_void','assets/images/element_generic.png',discovered: true,discoveryOrder: 0,possibleCombinations: [],),
+GameElement('time','element_time','assets/images/element_generic.png',discovered: true,discoveryOrder: 1,possibleCombinations: [],),
+GameElement('energy','element_energy','assets/images/element_generic.png',discovered: true,discoveryOrder: 2,possibleCombinations: [],),
+GameElement('atom','element_atom','assets/images/element_generic.png',discovered: false,possibleCombinations: [{'parent1Id': 'void', 'parent2Id': 'energy'}],),
+GameElement('gas','element_gas','assets/images/element_generic.png',discovered: false,possibleCombinations: [{'parent1Id': 'void', 'parent2Id': 'atom'}],),
+GameElement('element','element_element','assets/images/element_generic.png',discovered: false,possibleCombinations: [{'parent1Id': 'atom', 'parent2Id': 'atom'}],),
+GameElement('cellular','element_cellular','assets/images/element_generic.png',discovered: false,possibleCombinations: [{'parent1Id': 'element', 'parent2Id': 'energy'}],),
+GameElement('pressure','element_pressure','assets/images/element_generic.png',discovered: false,possibleCombinations: [{'parent1Id': 'time', 'parent2Id': 'energy'}],),
+GameElement('star','element_star','assets/images/element_generic.png',discovered: false,possibleCombinations: [{'parent1Id': 'gas', 'parent2Id': 'pressure'}],),
+GameElement('bacteria','element_bacteria','assets/images/element_generic.png',discovered: false,possibleCombinations: [{'parent1Id': 'cellular', 'parent2Id': 'cellular'}],),
 ];
