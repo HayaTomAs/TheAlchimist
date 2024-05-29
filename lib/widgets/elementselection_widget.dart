de3d6804f022cf4929c0864376773a5c749f1c7f
Thomas Darmengeat <thomas.darmengeat@gmail.com>
2024-05-29 19:27:15 +0200

    Changement de l'affichage des elements

--- lib/widgets/elementselection_widget.dart
+++ lib/widgets/elementselection_widget.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '/models/elements_base.dart';
-import '/models/elements_list.dart';
 import '/services/localization.dart';
 import '/services/logger_service.dart';
 import '/services/element_provider.dart';
@@ -14,10 +13,10 @@ class ElementSelectionScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final elementsProvider = Provider.of<ElementsProvider>(context);
     LoggerService.debug('Building ElementSelectionScreen widget');
+    final GlobalKey stackKey = GlobalKey(); // Créer un GlobalKey pour le Stack
 
-    int totalElements = initialElements.length;
+    int totalElements = getInitialElements().length;
     int discoveredElements = elementsProvider.availableElements.length;
-    final int columns = (MediaQuery.of(context).size.width / 100).floor();
 
     return Scaffold(
       appBar: AppBar(
@@ -33,39 +32,58 @@ class ElementSelectionScreen extends StatelessWidget {
             ),
             const SizedBox(height: 16),
             Expanded(
-              child: GridView.builder(
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: columns,
-                  childAspectRatio: 1,
-                ),
-                itemCount: elementsProvider.availableElements.length,
-                itemBuilder: (context, index) {
-                  final element = elementsProvider.availableElements[index];
-                  return GestureDetector(
-                    onDoubleTap: () {
-                      LoggerService.debug('Double click');
-                      elementsProvider.combineElements(element, element);
-                    },
-                    child: Draggable<GameElement>(
-                      data: element,
-                      feedback: Material(
-                        color: Colors.transparent,
-                        child: ElementWidget(element: element),
-                      ),
-                      childWhenDragging: Opacity(
-                        opacity: 0.5,
-                        child: ElementWidget(element: element),
-                      ),
-                      child: DragTarget<GameElement>(
-                        onAcceptWithDetails: (details) {
-                          LoggerService.debug('Reception element');
-                          elementsProvider.combineElements(element, details.data);
-                        },
-                        builder: (context, candidateData, rejectedData) {
-                          return ElementWidget(element: element);
-                        },
-                      ),
-                    ),
+              child: LayoutBuilder(
+                builder: (context, constraints) {
+                  return Stack(
+                    key: stackKey, // Assigner la clé au Stack
+                    children: elementsProvider.availableElements.map((element) {
+                      final position = elementsProvider.getElementPosition(element);
+                      return Positioned(
+                        left: position.dx,
+                        top: position.dy,
+                        child: GestureDetector(
+                          onDoubleTap: () {
+                            LoggerService.debug('Double click');
+                            elementsProvider.combineElements(element, element);
+                          },
+                          child: Draggable<GameElement>(
+                            data: element,
+                            feedback: Material(
+                              color: Colors.transparent,
+                              child: Opacity(
+                                opacity: 0.7,
+                                child: ElementWidget(element: element),
+                              ),
+                            ),
+                            childWhenDragging: Opacity(
+                              opacity: 0.5,
+                              child: ElementWidget(element: element),
+                            ),
+                            onDragEnd: (details) {
+                              // Obtenir la position du Stack par rapport à l'écran
+                              final stackRenderBox = stackKey.currentContext?.findRenderObject() as RenderBox?;
+                              final stackPosition = stackRenderBox?.localToGlobal(Offset.zero) ?? Offset.zero;
+
+                              // Calculer la position relative à l'intérieur du Stack
+                              Offset adjustedPosition = details.offset - stackPosition;
+
+                              Offset constrainedPosition =
+                                  _constrainPosition(adjustedPosition, constraints.maxWidth, constraints.maxHeight);
+                              elementsProvider.updateElementPosition(element, constrainedPosition);
+                            },
+                            child: DragTarget<GameElement>(
+                              onAcceptWithDetails: (details) {
+                                LoggerService.debug('Received element');
+                                elementsProvider.combineElements(element, details.data);
+                              },
+                              builder: (context, candidateData, rejectedData) {
+                                return ElementWidget(element: element);
+                              },
+                            ),
+                          ),
+                        ),
+                      );
+                    }).toList(),
                   );
                 },
               ),
@@ -79,5 +97,17 @@ class ElementSelectionScreen extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  Offset _constrainPosition(Offset position, double maxWidth, double maxHeight) {
+    // Taille de l'élément (peut être ajustée selon la taille réelle de votre élément)
+    const double elementWidth = 100;
+    const double elementHeight = 100;
+
+    // Contraindre la position pour rester à l'intérieur des limites
+    double constrainedX = position.dx.clamp(0, maxWidth - elementWidth);
+    double constrainedY = position.dy.clamp(0, maxHeight - elementHeight);
+
+    return Offset(constrainedX, constrainedY);
   }
 }
